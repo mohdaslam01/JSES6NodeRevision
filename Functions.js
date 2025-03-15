a89736578a7bace80404373633c31d8d56046185
MOHAMMED ASLAM <aslam.mohd14338@gmail.com>
2025-03-15 11:27:28 -0400

    node commit 1, Alhamdulillah

---
+++ Functions.js
@@ -0,0 +1,7 @@
+let user = () => console.log('It is time to rise up, insha Allah, to control with the permission of Allah');
+var user1 = function(uid){
+    console.log('Start up ' + uid);
+}
+
+user();
+user1('Mohammed Aslam');
