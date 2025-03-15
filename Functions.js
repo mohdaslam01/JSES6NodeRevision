543c705aa92c021a7f012d3c6ae8a57bd2851afb
MOHAMMED ASLAM <aslam.mohd14338@gmail.com>
2025-03-15 11:54:12 -0400

    setTimeout and setInterval

--- Functions.js
+++ Functions.js
@@ -2,6 +2,21 @@ let user = () => console.log('It is time to rise up, insha Allah, to control wit
 var user1 = function(uid){
     console.log('Start up ' + uid);
 }
-
+var sumoftwo = (a, b) => {
+    console.log(a + ' + ' + b + ' = ' + (a + b));
+}
+//settimeout is inbuild function of javascript
+setTimeout(
+    function(){
+        console.log('Alhamdulillah')
+    }, 5000
+)
+//setInterval
+setInterval(
+    function(){
+        console.log('Alhamdulillah')
+    }, 5000
+)
 user();
 user1('Mohammed Aslam');
+sumoftwo(1,2);
